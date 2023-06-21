a9244ebe454f1611ff2944f09fe6dfedbd02d6ea
Satyam Tripathi <st17931@gmail.com>
2023-06-21 09:40:57 +0530

    Episode 1 all concepts

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const element = React.createElement('h1',{style:"red"},"Hi this is Satyam Tripathi");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
